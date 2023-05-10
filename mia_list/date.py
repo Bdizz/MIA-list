a4b2faa60d0a766f818196ea9e1e949acbdb0473
Bdizz <42521740+Bdizz@users.noreply.github.com>
2023-05-10 02:07:36 -0400

    Update date.py

--- mia_list/date.py
+++ mia_list/date.py
@@ -5,13 +5,10 @@ COLUMN_BUFFER = 6
 
 def get_week_dates():
     """
-    - The function will get the date using the datetime module
+    Get the start and finish dates of the current week.
 
-    - Then get the dates for the week using the timedetla function
-
-    - Lastly make a list with the start and finish dates of the week
-
-    :return: A list consisting of the start and finish day dates for the week
+    Returns:
+        list: A list containing the start and finish day dates for the week.
     """
     today = date.today()
     start = today - timedelta(days=today.weekday())
@@ -23,11 +20,10 @@ def get_week_dates():
 
 def get_last_week():
     """
-    - The function get last Monday and Sundays dates
+    Get the start and finish dates of the previous week.
 
-    - Then put them into a list
-
-    :return: a list containing last Monday and Sundays dates.
+    Returns:
+        list: A list containing the start and finish day dates of the last week.
     """
     today = date.today()
     start = today - timedelta(days=today.weekday()) + timedelta(days=2, weeks=-1)
@@ -39,14 +35,15 @@ def get_last_week():
 
 def adjust_date_list(date_range):
     """
-    - This function will adjust the date range list so that the range of numbers is aligned with the MIA LIST
+    Adjust the date range list so that the range of numbers is aligned with the MIA LIST.
 
-    :param date_range: Range of dates (Monday : Sunday)
-    :return: An adjusted date range that is accurate with the MIA LIST.
+    Args:
+        date_range (list): Range of dates (Monday:Sunday).
+
+    Returns:
+        list: An adjusted date range that is accurate with the MIA LIST.
     """
-
-    date_range[0] = date_range[0] + COLUMN_BUFFER
-    date_range[1] = date_range[1] + COLUMN_BUFFER
+    date_range[0] += COLUMN_BUFFER
+    date_range[1] += COLUMN_BUFFER
 
     return date_range
-
